b5380f8bf416e29b563ec276f3d195b8ae6ff356
Callum Messiter <cmessiter@hotmail.co.uk>
2017-05-27 23:12:01 +0200

    Build MVC skeleton and specify endpoints

--- controllers/RestaurantController.js
+++ controllers/RestaurantController.js
@@ -1,9 +1,8 @@
 const express = require('express');
 const router = express.Router();
-const Users = require('../models/Users');
 
 router.get('/', (req, res, next) => {
-	res.send('Users');
+	res.send('Restaurant');
 });
 
 module.exports = router;
--- routes/api.js
+++ routes/api.js
@@ -3,8 +3,30 @@ const router = express.Router();
 
 // Import any api-specific controllers here
 const UserController = require('../controllers/UserController');
+const RestaurateurController = require('../controllers/RestaurateurController');
+const DinerController = require('../controllers/DinerController');
+const AdminController = require('../controllers/AdminController');
+const AuthController = require('../controllers/AuthController');
+const RestaurantController = require('../controllers/RestaurantController');
+const MenuController = require('../controllers/MenuController');
+const SectionController = require('../controllers/SectionController');
+const CategoryController = require('../controllers/CategoryController');
+const ItemController = require('../controllers/ItemController');
+const TransactionController = require('../controllers/TransactionController');
+const EmailTemplatesController = require('../controllers/EmailTemplatesController');
 
 // Append controller routes to the '/api' endpoint here
-router.use('/users', UserController); // Requests to '/api/users' => UserController, and its respective routes
+router.use('/users', UserController);
+router.use('/restaurateurs', RestaurateurController);
+router.use('/diners', DinerController);
+router.use('/admins', AdminController);
+router.use('/auth', AuthController);
+router.use('/restaurants', RestaurantController);
+router.use('/menus', MenuController);
+router.use('/sections', SectionController);
+router.use('/categories', CategoryController);
+router.use('/items', ItemController);
+router.use('/transactions', TransactionController);
+router.use('/emailTemplates', EmailTemplatesController);
 
 module.exports = router;

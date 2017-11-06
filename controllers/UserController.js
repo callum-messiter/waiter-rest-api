0ccf4236196472ae5606262fed158c6222665f29
Callum Messiter <cmessiter@hotmail.co.uk>
2017-11-07 00:32:15 +0100

    Implement dev message for user endpoints

--- controllers/UserController.js
+++ controllers/UserController.js
@@ -25,7 +25,10 @@ router.get('/', (req, res, next) => {
 	// Check that the user is a waiterAdmin
 	Users.getAllUsers((err, users) => {
 		if(err) {
-			ResponseHelper.sendError(res, 500, 'get_all_users_query_error', err);
+			ResponseHelper.sendError(res, 500, 'get_all_users_query_error',
+				ResponseHelper.msg.sql+err.code,
+				ResponseHelper.msg.default
+			);
 		} else {
 			ResponseHelper.sendSuccess(res, 200, {users: users});
 		}
@@ -39,7 +42,9 @@ router.get('/:userId', (req, res, next) => {
 	// Check that the request contains a token, and the Id of the user whose details are to be retrieved
 	if(!req.headers.authorization || !req.params.userId) {
 		ResponseHelper.sendError(res, 404, 'missing_required_params', 
-			'The server was expecting a userId and a token. At least one of these parameters was missing from the request.');
+			"The server was expecting the req param 'userId', and the 'Authorization' header.",
+			ResponseHelper.msg.default
+		);
 	} else {
 		const userId = req.params.userId;
 		const token = req.headers.authorization;
@@ -47,21 +52,30 @@ router.get('/:userId', (req, res, next) => {
 		Auth.verifyToken(token, (err, decodedpayload) => {
 			if(err) {
 				ResponseHelper.sendError(res, 401, 'invalid_token', 
-					'The server determined that the token provided in the request is invalid. It likely expired - try logging in again.');
+					'The server determined that the token provided in the request is invalid. It likely expired - log the user out.',
+					'Your session has expired. Please log in again.' // Not needed
+				);
 			} else {
 				const requesterId = decodedpayload.userId;
 
 				Users.getUserById(userId, (err, result) => {
 					if(err) {
-						ResponseHelper.sendError(res, 500, 'get_user_query_error', err);
+						ResponseHelper.sendError(res, 500, 'get_user_query_error',
+							ResponseHelper.msg.sql+err.code,
+							ResponseHelper.msg.default
+						);
 					} else if(result.length < 1) {
 						ResponseHelper.sendError(res, 404, 'user_not_found', 
-							'There are no users matching the ID provided.');
+							'There are no users matching the ID provided.',
+							ResponseHelper.msg.default
+						);
 					} else {
 						// User details can be accessed only by the owner, or by an internal admin
 						if(requesterId != userId) {
 							ResponseHelper.sendError(res, 401, 'unauthorised', 
-								'A user\'s details can be accessed only by the owner, or by admins.');
+								'A user\'s details can be accessed only by the owner, or by admins.',
+								"Sorry, you don't have permission to do that!"
+							);
 						} else {
 							// Return only insensitive user information
 							user = {
@@ -86,17 +100,21 @@ router.get('/:userId', (req, res, next) => {
 **/
 router.post('/create', (req, res, next) => {
 	// No token required, and no access restriction
-	userRolesObject = UserRoles.roleIDs;
-	userType = req.body.userType;
+	const userRolesObject = UserRoles.roleIDs;
 	// Check the subroute is set
-	if(!userType) {
+	if(!req.body.userType) {
 		ResponseHelper.sendError(res, 404, 'missing_required_params', 
-			'Server was expecting a parameter that specifies the type of user to be created.');
+			'The server was expecting the req param "userType".',
+			ResponseHelper.msg.default
+		);
 	} else {
+		const userType = req.body.userType;
 		// Check that the subroute is valid (the user has specified a valid user type)
 		if(!userRolesObject.hasOwnProperty(userType)) {
-			ResponseHelper.sendError(res, 404, 'invalid_subroute', 
-				'An invalid user type was specified in the subroute.');
+			ResponseHelper.sendError(res, 404, 'invalid_user_type', 
+				'" + ' + userType + '" is not a valid user type.',
+				ResponseHelper.msg.default
+			);
 		} else {
 			userRole = userRolesObject[userType]; // e.g. roleIDs['admin'] = 900
 			// Check that the request contains all required user details
@@ -105,12 +123,17 @@ router.post('/create', (req, res, next) => {
 			   !req.body.firstName || !req.body.lastName || !req.body.restaurantName
 			) {
 				ResponseHelper.sendError(res, 404, 'missing_required_params', 
-					'The server was expecting an email, password, first name, last name and restaurantName. At least of of these parameters was missing from the request.');
+					'The server was expecting the req params "email", "password", "firstName", "lastName", and "restaurantName".',
+					ResponseHelper.msg.default
+				);
 			} else {
 				// Check if the email is already registered
 				Users.isEmailRegistered(req.body.email, (err, result) => {
 					if(err) {
-						ResponseHelper.sendError(res, 500, 'email_registered_query_error', err);
+						ResponseHelper.sendError(res, 500, 'email_registered_query_error',
+							ResponseHelper.msg.sql+err.code,
+							ResponseHelper.msg.default
+						);
 					} else {
 						if(result[0].matches > 0) {
 							ResponseHelper.sendError(res, 409, 
@@ -119,7 +142,10 @@ router.post('/create', (req, res, next) => {
 							// Hash the password
 							Users.hashPassword(req.body.password, (err, hashedPassword) => {
 								if(err) {
-									ResponseHelper.sendError(res, 500, 'bcrypt_error', err);
+									ResponseHelper.sendError(res, 500, 'bcrypt_error', 
+										'There was an error with the bcrypt package: ' + err,
+										ResponseHelper.msg.default
+									);
 								} else {
 									// Create user object with hashed password
 									const user = {
@@ -132,7 +158,10 @@ router.post('/create', (req, res, next) => {
 									// Add the new user to the db
 									Users.createNewUser(user, (err, result) => {
 										if(err) {
-											ResponseHelper.sendError(res, 500, 'create_user_query_error', err);
+											ResponseHelper.sendError(res, 500, 'create_user_query_error',
+												ResponseHelper.msg.sql+err.code,
+												ResponseHelper.msg.default
+											);
 										} else {
 											const userId = user.userId;
 											// Set the user's role, which we get from the userRolesObject, using the specified userType
@@ -144,7 +173,10 @@ router.post('/create', (req, res, next) => {
 
 											UserRoles.setUserRole(userDetails, (err) => {
 												if(err) {
-													ResponseHelper.sendError(res, 500, 'set_user_role_query_error', err);
+													ResponseHelper.sendError(res, 500, 'set_user_role_query_error',
+														ResponseHelper.msg.sql+err.code,
+														ResponseHelper.msg.default
+													);
 												} else {
 													// Create the user's restaurant, and the default menu with default categories
 													const restaurant = {
@@ -160,7 +192,10 @@ router.post('/create', (req, res, next) => {
 
 													Restaurants.createRestaurantWithDefaultMenu(restaurant, menu, (err, result) => {
 														if(err) {
-															ResponseHelper.sendError(res, 500, 'create_restaurant_and_menu_query_error', err);
+															ResponseHelper.sendError(res, 500, 'create_restaurant_and_menu_query_error',
+																ResponseHelper.msg.sql+err.code,
+																ResponseHelper.msg.default
+															);
 														} else {
 															ResponseHelper.sendSuccess(res, 201, {
 																user: {
@@ -246,7 +281,9 @@ router.put('/deactivate/:userId', (req, res, next) => {
 	// Check that the request contains a token, and the Id of the user whose details are to be deactivated
 	if(!req.headers.authorization || !req.params.userId) {
 		ResponseHelper.sendError(res, 404, 'missing_required_params', 
-			'The server was expecting a userId and a token. At least one of these parameters was missing from the request.');
+			"The server was expecting the req param 'userId', and the 'Authorization' header.",
+			ResponseHelper.msg.default
+		);
 	} else {
 		const userId = req.params.userId;
 		const token = req.headers.authorization;
@@ -254,7 +291,9 @@ router.put('/deactivate/:userId', (req, res, next) => {
 		Auth.verifyToken(token, (err, decodedpayload) => {
 			if(err) {
 				ResponseHelper.sendError(res, 401, 'invalid_token', 
-					'The server determined that the token provided in the request is invalid. It likely expired - try logging in again.');
+					'The server determined that the token provided in the request is invalid. It likely expired - log the user out.',
+					'Your session has expired. Please log in again.' // Not needed
+				);
 			} else {
 				const requesterRole = decodedpayload.userRole;
 				const requesterId = decodedpayload.userId;
@@ -262,28 +301,42 @@ router.put('/deactivate/:userId', (req, res, next) => {
 				// A user can be deactivated only by the owner, or by an internal admin
 				if(requesterId != userId && requesterRole != waiterAdmin) {
 					ResponseHelper.sendError(res, 401, 'unauthorised', 
-						'A user account can be deactivated only by the owner, or by an internal admin.');
+						'A user account can be deactivated only by the owner, or by an internal admin.',
+						"Sorry, you don't have permission to do that!"
+					);
 				} else {
 					// Before deactivating the user, check if the account is already active
 					Users.getUserById(userId, (err, user) => {
 						if(err) {
-							ResponseHelper.sendError(res, 500, 'get_user_query_error', err);
+							ResponseHelper.sendError(res, 500, 'get_user_query_error',
+								ResponseHelper.msg.sql+err.code,
+								ResponseHelper.msg.default
+							);
 						} else if(user.length < 1) {
 							ResponseHelper.sendError(res, 404, 'user_not_found', 
-								'A user with the specified Id could not be found.');
+								'A user with the specified Id could not be found.',
+								ResponseHelper.msg.default
+							);
 						} else {
 							const IsActive = user[0].IsActive;
 							// Check if the user is active
 							if(!IsActive) {
 								ResponseHelper.sendError(res, 409, 'user_already_inactive', 
-									'The server determined that the specified user account is aready inactive. You cannot deactivate an inactive account.');
+									'The server determined that the specified user account is aready inactive. You cannot deactivate an inactive account.',
+									'This account has already been deactivated. To re-activate it, click here.'
+								);
 							} else {
 								Users.deactivateUser(userId, (err, result) => {
 									if(err) {
-										ResponseHelper.sendError(res, 500, 'deactivate_user_query_error', err);
+										ResponseHelper.sendError(res, 500, 'deactivate_user_query_error',
+											ResponseHelper.msg.sql+err.code,
+											ResponseHelper.msg.default
+										);
 									} else if(result.affectedRows < 1) {
 										ResponseHelper.sendError(res, 404, 'user_not_deactivated', 
-											'The query was executed successfully but the user account was not deactivated.');
+											'The query was executed successfully but the user account was not deactivated.',
+											ResponseHelper.msg.default
+										);
 									} else {
 										ResponseHelper.sendSuccess(res, 200);
 									}	
@@ -303,11 +356,15 @@ router.put('/deactivate/:userId', (req, res, next) => {
 router.put('/updateDetails/:userId', (req, res, next) => {
 	if(!req.headers.authorization || !req.params.userId) {
 		ResponseHelper.sendError(res, 404, 'missing_required_params', 
-			"The server was expecting an 'authorization' header, and a userId. At least one of these params was missing.");
+			"The server was expecting the req param 'userId', and the 'Authorization' header.",
+			ResponseHelper.msg.default
+		);
 	} else {
 		if(!req.body.firstName && !req.body.lastName) {
 			ResponseHelper.sendError(res, 404, 'missing_required_params', 
-				'The server received zero valid parameters to be updated.');
+				'The server received zero valid parameters to be updated.',
+				ResponseHelper.msg.default
+			);
 		} else {
 			const token = req.headers.authorization;
 			const userId = req.params.userId;
@@ -316,24 +373,33 @@ router.put('/updateDetails/:userId', (req, res, next) => {
 			const requestDataIsValid = RequestHelper.checkRequestDataIsValid(userDetails, modifiableUserDetails, res);
 			if(requestDataIsValid !== true) {
 				ResponseHelper.sendError(res, 422, 'invalid_data_params', 
-					"The data parameter '" + requestDataIsValid + "' is not a valid parameter for the resource in question.");
+					"The data parameter '" + requestDataIsValid + "' is not a valid parameter for the resource in question.",
+					ResponseHelper.msg.default
+				);
 			} else {
 				Auth.verifyToken(token, (err, decodedpayload) => {
 					if(err) {
 						ResponseHelper.sendError(res, 401, 'invalid_token', 
-							'The server determined that the token provided in the request is invalid. It likely expired - try logging in again.');
+							'The server determined that the token provided in the request is invalid. It likely expired - log the user out.',
+							'Your session has expired. Please log in again.' // Not needed
+						);
 					} else {
 						const ownerId = userId;
 						const requesterId = decodedpayload.userId;
 						// Menus can only be modified by the menu owner
 						if(requesterId != ownerId) {
 							ResponseHelper.sendError(res, 401, 'unauthorised', 
-								'A user can modify only their own details.');
+								'A user can modify only their own details.',
+								"Sorry, you don't have permission to do that!"
+							);
 						} else {
 							// Update user details
 							Users.updateUserDetails(userId, userDetails, (err, result) => {
 								if(err) {
-									ResponseHelper.sendError(res, 500, 'update_user_query_error', err);
+									ResponseHelper.sendError(res, 500, 'update_user_query_error',
+										ResponseHelper.msg.sql+err.code,
+										ResponseHelper.msg.default
+									);
 								} else if(result.changedRows < 1) {
 									QueryHelper.diagnoseQueryError(result, res);
 								} else {
@@ -355,10 +421,14 @@ router.put('/updatePassword/:userId', (req, res, next) => {
 	// Check the request contains the required data
 	if(!req.headers.authorization || !req.params.userId) {
 		ResponseHelper.sendError(res, 404, 'missing_required_params', 
-			"The server was expecting an 'authorization' header, and a userId. At least one of these params was missing.");
+			"The server was expecting the req param 'userId', and the 'Authorization' header.",
+			ResponseHelper.msg.default
+		);
 	} else if(!req.body.currentPassword || !req.body.newPassword) {
 		ResponseHelper.sendError(res, 404, 'missing_required_params', 
-			'The server was expecting a newPassword parameter and a currentPassword parameter.');
+			'The server was expecting the req params "newPassword" and "currentPassword".',
+			ResponseHelper.msg.default
+		);
 	} else {
 		const token = req.headers.authorization;
 		const userId = req.params.userId;
@@ -368,27 +438,38 @@ router.put('/updatePassword/:userId', (req, res, next) => {
 		Auth.verifyToken(token, (err, decodedpayload) => {
 			if(err) {
 				ResponseHelper.sendError(res, 401, 'invalid_token', 
-					'The server determined that the token provided in the request is invalid. It likely expired - try logging in again.');
+					'The server determined that the token provided in the request is invalid. It likely expired - log the user out.',
+					'Your session has expired. Please log in again.' // Not needed'
+				);
 			} else {
 				const ownerId = userId;
 				const requesterId = decodedpayload.userId;
 				// A user can update only their own password
 				if(requesterId != ownerId) {
 					ResponseHelper.sendError(res, 401, 'unauthorised', 
-						'A user can modify only their own details.');
+						'A user can modify only their own details.',
+						"Sorry, you don't have permission to do that!"
+					);
 				} else {
 					// Check that the user has entered a password that is different from their current password
 					if(newPassword == currentPassword) {
 						ResponseHelper.sendError(res, 409, 'password_conflict', 
-							'The new password provided is the same as the user\'s current password.');
+							'The new password provided is the same as the user\'s current password.',
+							'The new password you entered is already assigned to your account. Please log in, or provide a different new password.'
+						);
 					} else {
 						// Get the user's current hashed password
 						Users.getUserById(userId, (err, result) => {
 							if(err) {
-								ResponseHelper.sendError(res, 500, 'get_user_query_error', err);
+								ResponseHelper.sendError(res, 500, 'get_user_query_error',
+									ResponseHelper.msg.sql+err.code,
+									ResponseHelper.msg.default
+								);
 							} else if(result.length < 1) {
 								ResponseHelper.sendError(res, 404, 'user_not_found', 
-									'The query returned zero results. It is likely that a user with the specified ID does not exist.');
+									'The query returned zero results. It is likely that a user with the specified ID does not exist.',
+									ResponseHelper.msg.default
+								);
 							} else {
 								const currentHashedPass = result[0].password;
 								const currentPlainTextPass = currentPassword;
@@ -396,20 +477,30 @@ router.put('/updatePassword/:userId', (req, res, next) => {
 								// Check that the user has entered their current password correctly
 								Users.checkPassword(currentPlainTextPass, currentHashedPass, (err, passwordsMatch) => {
 									if(err) {
-										ResponseHelper.sendError(res, 500, 'bcrypt_error', err);
+										ResponseHelper.sendError(res, 500, 'bcrypt_error',
+											ResponseHelper.msg.sql+err.code,
+											ResponseHelper.msg.default
+										);
 									} else if(!passwordsMatch) {
 										ResponseHelper.sendError(res, 401, 'invalid_password', 
-											'The currentPassword provided does not match the user\'s current password in the database.');
+											'The currentPassword provided does not match the user\'s current password in the database.',
+											'The password you entered is incorrect. Please enter your current password, or reset it.');
 									} else if(passwordsMatch){
 										// Hash the new password
 										Users.hashPassword(newPassword, (err, newHashedPassword) => {
 											if(err) {
-												ResponseHelper.sendError(res, 500, 'bcrypt_error', err);
+												ResponseHelper.sendError(res, 500, 'bcrypt_error',
+													'There was an error with the bcrypt package: ' + err,
+													ResponseHelper.msg.default
+												);
 											} else {
 												// Update user details
 												Users.updateUserPassword(userId, newHashedPassword, (err, result) => {
 													if(err) {
-														ResponseHelper.sendError(res, 500, 'update_password_query_error', err);
+														ResponseHelper.sendError(res, 500, 'update_password_query_error',
+															ResponseHelper.msg.sql+err.code,
+															ResponseHelper.msg.default
+														);
 													} else if(result.changedRows < 1) {
 														QueryHelper.diagnoseQueryError(result, res);
 													} else {
